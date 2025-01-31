72366d89e1e44f14056865db035b46a5bece3f42
Keko <kekonline@gmail.com>
2025-01-31 20:52:30 +0100

    semaphore pattern

--- 7. Go Patterns/9. Semaphore Pattern/app.go
+++ 7. Go Patterns/9. Semaphore Pattern/app.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	const maxWorkers = 3                         // Limit concurrency to 3 workers
+	semaphore := make(chan struct{}, maxWorkers) // Buffered channel as semaphore
+	var wg sync.WaitGroup
+
+	tasks := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	for _, task := range tasks {
+		wg.Add(1)
+		semaphore <- struct{}{} // Acquire semaphore
+
+		go func(task int) {
+			defer wg.Done()
+			defer func() { <-semaphore }() // Release semaphore
+
+			fmt.Printf("Processing task %d\n", task)
+			time.Sleep(2 * time.Second) // Simulate work
+		}(task)
+	}
+
+	wg.Wait()
+	fmt.Println("All tasks completed!")
 }
