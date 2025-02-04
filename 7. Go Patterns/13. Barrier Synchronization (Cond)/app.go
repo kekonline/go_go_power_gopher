40875fa20224ad24521b79ad240ea8a4e1027342
Keko <kekonline@gmail.com>
2025-02-04 20:18:00 +0100

    barrier sync with condition

--- 7. Go Patterns/13. Barrier Synchronization (Cond)/app.go
+++ 7. Go Patterns/13. Barrier Synchronization (Cond)/app.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const numWorkers = 3
+
+func worker(id int, cond *sync.Cond, counter *int, mutex *sync.Mutex) {
+	fmt.Printf("Worker %d: Doing some work...\n", id)
+	time.Sleep(time.Duration(id) * time.Second) // Simulate work
+
+	mutex.Lock()
+	*counter++
+	if *counter == numWorkers {
+		fmt.Println("All workers reached the barrier. Broadcasting signal...")
+		cond.Broadcast() // Wake up all waiting goroutines
+	} else {
+		fmt.Printf("Worker %d: Waiting at the barrier...\n", id)
+		cond.Wait() // Wait for the signal
+	}
+	mutex.Unlock()
+
+	fmt.Printf("Worker %d: Passed the barrier and continuing...\n", id)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var mutex sync.Mutex
+	cond := sync.NewCond(&mutex)
+	counter := 0
+
+	for i := 1; i <= numWorkers; i++ {
+		go worker(i, cond, &counter, &mutex)
+	}
+
+	time.Sleep(5 * time.Second) // Ensure main doesn't exit early
 }
